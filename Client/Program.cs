251ed9b102df6e99379301a0bbc6d64c7cb8e269
Kevin Pilch <me@pilchie.com>
2021-08-14 10:23:24 -0700

    Add an auth message handler for gRPC client

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,5 @@
 using BuildWatch.Client;
 using BuildWatch.Shared;
-using Grpc.Net.Client;
 using Grpc.Net.Client.Web;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
@@ -9,21 +8,29 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
-builder.Services.AddHttpClient("BuildWatch.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+builder.Services
+    .AddHttpClient("BuildWatch.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
     .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
-builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BuildWatch.ServerAPI"));
+builder.Services
+    .AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BuildWatch.ServerAPI"));
 
-builder.Services.AddMsalAuthentication(options =>
-{
-    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
-    options.ProviderOptions.DefaultAccessTokenScopes.Add("api://177e5287-d277-4d65-8e7b-77135c831bed/API.Access");
-});
+builder.Services
+    .AddMsalAuthentication(options =>
+    {
+        builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+        options.ProviderOptions.DefaultAccessTokenScopes.Add("api://177e5287-d277-4d65-8e7b-77135c831bed/API.Access");
+    });
 
-builder.Services.AddGrpcClient<WeatherForecasts.WeatherForecastsClient>((services, options) =>
-{
-    options.Address = new Uri(ServiceProviderServiceExtensions.GetRequiredService<NavigationManager>(services).BaseUri);
-}).ConfigurePrimaryHttpMessageHandler(() => new GrpcWebHandler(new HttpClientHandler()));
+builder.Services
+    .AddGrpcClient<WeatherForecasts.WeatherForecastsClient>((services, options) =>
+    {
+        options.Address = new Uri(ServiceProviderServiceExtensions.GetRequiredService<NavigationManager>(services).BaseUri);
+    })
+    .ConfigurePrimaryHttpMessageHandler(services =>
+     {
+         return new GrpcWebHandler(new HttpClientHandler());
+     }).AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
 await builder.Build().RunAsync();

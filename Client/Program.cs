0fe646c29b9b12f1b6f5b7c1d8892d443c9e852b
Kevin Pilch <me@pilchie.com>
2021-08-14 10:25:15 -0700

    Remove HttpClientFactory setup

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,14 +9,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
 builder.Services
-    .AddHttpClient("BuildWatch.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
-    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-
-// Supply HttpClient instances that include access tokens when making requests to the server project
-builder.Services
-    .AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BuildWatch.ServerAPI"));
-
-builder.Services
     .AddMsalAuthentication(options =>
     {
         builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
